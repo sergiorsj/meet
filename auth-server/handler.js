5a4186775282e06baec7d1f9aa26e7cd67909378
sergiorsj11 <sergiorsj11@gmail.com>
2024-08-05 21:03:51 -0400

    Updates

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -14,7 +14,7 @@ const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-  "https://YOUR_GITHUB_USERNAME.github.io/meet/"
+  "https://sergiorsj.github.io/meet/"
 ];
 
 const oAuth2Client = new google.auth.OAuth2(
@@ -82,6 +82,57 @@ module.exports.getAccessToken = async (event) => {
       };
     });
 };
+
+
+module.exports.getCalendarEvents = async (event) => {
+  // Decode authorization code extracted from the URL query
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+
+  oAuth2Client.setCredentials({ access_token });
+  
+  return new Promise((resolve, reject) => {
+    /**
+     *  Exchange authorization code for access token with a “callback” after the exchange,
+     *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
+     */
+
+    calendar.events.list(
+      {
+        calendarId: CALENDAR_ID,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  })
+    .then((results) => {
+      // Respond with OAuth token 
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({ events: results.data.items }),
+      };
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+};
+
 
 exports.hello = async (event) => {
   return {
